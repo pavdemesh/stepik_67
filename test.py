4adb68b97f262282a3561cf14753310302401f0b
pavdemesh <pavdemesh@gmail.com>
2020-10-19 19:53:07 +0300

    test

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 x = int(input('Enter the number of minutes to sleep: '))
-print('Hours to sleep:', x // 60)
-print('Minutes to sleep:', x % 60)
+h = int(input('Hours to add: '))
+m = int(input('Minutes to add: '))
+
+add_h = (x % 60 + m) // 60
+print(add_h)
+print((x % 60 + m) % 60)
