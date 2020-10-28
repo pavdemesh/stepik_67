b8654624bffbd2d40af73fb9943b8f9bdd19ec19
pavdemesh <pavdemesh@gmail.com>
2020-10-28 19:58:06 +0200

    1

---
+++ stepik_course_67_ex_2_3.py
@@ -0,0 +1,12 @@
+a, b, c, d = int(input()), int(input()), int(input()), int(input())
+
+print('\t', end='')
+for i in range(c, d + 1):
+    print(i, end='\t')
+print()
+
+for j in range(a, b + 1):
+    print(j, end='\t')
+    for k in range(c, d + 1):
+        print(j * k, end='\t')
+    print()
